da04fac2de389524fdd5bf8d43667d3c5e2c33f7
JV <jvitorcocal@hotmail.com>
2022-11-21 19:43:03 -0300

    Added scroll into view feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import Navbar from "./Components/Navbar";
 import Imagens from "./Components/Imagens";
-// import Contatos from "./components/Contatos";
-// import Lotes from "./components/Lotes";
+import Contatos from "./Components/Contatos";
+import Lotes from "./Components/Lotes";
 
 export default function App() {
+  const imagens = useRef(null)
+  const lotes = useRef(null)
+  const contatos = useRef(null)
+
   return (
     <div className="mainPage">
-      <Navbar />
-      <Imagens />
-      {/* <Lotes /> */}
-      {/* <Contatos /> */}
+      <Navbar
+        fotosClick = {()=> imagens.current?.scrollIntoView({behavior: "smooth", block:"center"})}
+        lotesClick = {()=> lotes.current?.scrollIntoView({behavior: "smooth", block:"center"})}
+        contatoClick = {()=> contatos.current?.scrollIntoView({behavior: "smooth", block:"center"})}
+      />
+      
+      <div ref={imagens} className="">
+        <Imagens/>
+      </div>
+
+      <div ref={lotes}>
+        <Lotes />
+      </div>
+
+      <div ref={contatos}>
+        <Contatos />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import logoLoteamento from "../img/logoLoteamento.png"
 
-export default function Navbar(){
+export default function Navbar(props){
     return(
         <nav>
             <div className="nav--containner">
@@ -12,15 +12,15 @@ export default function Navbar(){
                     <div 
                         className="nav--infos"
                     >
-                        <div>
+                        <div onClick={props.fotosClick}>
                             <h3> Fotos e Videos </h3>
                         </div>
 
-                        <div>
+                        <div onClick={props.lotesClick}>
                             <h3> Lotes Disponíveis </h3>
                         </div>
 
-                        <div>
+                        <div onClick={props.contatoClick}>
                             <h3> Contato </h3>
                         </div>
                     </div>
