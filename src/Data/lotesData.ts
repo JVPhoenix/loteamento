cdb8234d60eda208fd983932d4b78d05e5082be4
JV <jvitorcocal@hotmail.com>
2023-01-13 09:01:54 -0300

    Data update

--- src/Data/lotesData.ts
+++ src/Data/lotesData.ts
@@ -15,7 +15,7 @@ const lotesData: {value: number, label: string, valor: number, tamanho: string}[
     // {value: 14 , label: 'Quadra 3 - Lote 15 ', valor: 18500.00, tamanho: "10x30 - 300m²"},
     // {value: 15 , label: 'Quadra 3 - Lote 16 ', valor: 18500.00, tamanho: "10x30 - 300m²"},
     
-    {value: 22 , label: 'Quadra 4 - Lote 4 ', valor: 13000.00, tamanho: "10x25 - 250m²"},
+    // {value: 22 , label: 'Quadra 4 - Lote 4 ', valor: 13000.00, tamanho: "10x25 - 250m²"},
     // {value: 24 , label: 'Quadra 4 - Lote 7 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
     {value: 25 , label: 'Quadra 4 - Lote 8 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
     // {value: 26 , label: 'Quadra 4 - Lote 9 ', valor: 17267.60, tamanho: "VARIÁVEL - 280m²"},
@@ -25,8 +25,8 @@ const lotesData: {value: number, label: string, valor: number, tamanho: string}[
     {value: 31 , label: 'Quadra 4 - Lote 15 ', valor: 17309.45, tamanho: "VARIÁVEL - 335m²"},
     {value: 32 , label: 'Quadra 4 - Lote 16 ', valor: 14725.95, tamanho: "VARIÁVEL - 285m²"},
 
-    {value: 34 , label: 'Quadra 5 - Lote 2 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
-    {value: 36 , label: 'Quadra 5 - Lote 4 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
+    // {value: 34 , label: 'Quadra 5 - Lote 2 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
+    // {value: 36 , label: 'Quadra 5 - Lote 4 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
     {value: 38 , label: 'Quadra 5 - Lote 7 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
     {value: 39 , label: 'Quadra 5 - Lote 8 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
     {value: 40 , label: 'Quadra 5 - Lote 9 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
