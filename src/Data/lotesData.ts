d38e464a5544f72e39010ed5454b17525f56282a
JV <jvitorcocal@hotmail.com>
2023-03-28 20:52:27 -0300

    Data Update

--- src/Data/lotesData.ts
+++ src/Data/lotesData.ts
@@ -3,14 +3,14 @@ const lotesData: {value: number, label: string, valor: number, tamanho: string}[
     {value: 0 , label: '---- DIGITE OU SELECIONE UM LOTE ----', valor: 0, tamanho: "0"},
     {value: 5 , label: 'Quadra 1 - Lote 8 ', valor: 20000.00, tamanho: "10x30 - 300m²"},
     {value: 6 , label: 'Quadra 1 - Lote 10 ', valor: 27751.50, tamanho: "34.5x30x6.5 - 450m²"},
-    {value: 7 , label: 'Quadra 1 - Lote 11 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
-    {value: 8 , label: 'Quadra 1 - Lote 12 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
-    {value: 9 , label: 'Quadra 1 - Lote 13 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
+    // {value: 7 , label: 'Quadra 1 - Lote 11 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
+    // {value: 8 , label: 'Quadra 1 - Lote 12 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
+    // {value: 9 , label: 'Quadra 1 - Lote 13 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
     // {value: 10 , label: 'Quadra 1 - Lote 14 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
 
     // {value: 12 , label: 'Quadra 2 - Lote 13 ', valor: 15500.00, tamanho: "10x30 - 300m²"},
 
-    {value: 14 , label: 'Quadra 3 - Lote 13 ', valor: 20000.00, tamanho: "10x30 - 300m²"},
+    // {value: 14 , label: 'Quadra 3 - Lote 13 ', valor: 20000.00, tamanho: "10x30 - 300m²"},
     // {value: 16 , label: 'Quadra 3 - Lote 16 ', valor: 18500.00, tamanho: "10x30 - 300m²"},
 
     // {value: 25 , label: 'Quadra 4 - Lote 7 ', valor: 20000.00, tamanho: "10x30 - 300m²"},
