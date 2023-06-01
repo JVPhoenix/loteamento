3e899319f3d6302ff31766a22f98a141dccbe144
jvphoenix <jvitorcocal@hotmail.com>
2023-06-01 13:08:30 -0300

    Bug fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title> Loteamento R. Martins </title>
       <body>
         <Main />
         <NextScript />
