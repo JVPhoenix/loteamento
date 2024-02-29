cf96d1a668e9e82abab101ae6089382cdf33e6df
JV <jvitorcocal@hotmail.com>
2024-02-28 22:57:13 -0300

    Deploy bug fix Fixed the deprecated NextJS configuration "images.domains", switched to "images.remotePatterns".

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["i.imgur.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 module.exports = nextConfig;
